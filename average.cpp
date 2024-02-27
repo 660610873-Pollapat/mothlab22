cd389a2bae495d5d89228ff49971d88e6d18bec6
660610873-Pollapat <polapat_chan@cmu.ac.th>
2024-02-27 15:08:49 +0700

    lab22 complete La

--- average.cpp
+++ average.cpp
@@ -1,10 +1,31 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average." << endl;
+        return 1;
+    }
 
-int main(int argc,char *argv[]){
+    double sum = 0;
+    int count = 0;
 
-    
+    for (int i = 1; i < argc; ++i) {
+        double number = atof(argv[i]);
+        sum += number;
+        count++;
+    }
 
-   return 0;
+    if (count == 0) {
+        std::cout << "No valid numbers entered." << endl;
+        return 1;
+    }
+
+    double average = sum / count;
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << count << " numbers = " << average << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
 }
